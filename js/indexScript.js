bda0cd45114ee7f9b3651dbcae6388feb2bc23ff
Samridh Tuladhar <t.samridh.86@gmail.com>
2016-06-13 17:26:50 +0545

    removed the extra page

--- js/indexScript.js
+++ js/indexScript.js
@@ -1,3 +1,15 @@
-function bio-click(){
-	
+function displayProject()
+{
+	document.getElementById('project-holder').style.display = 'block';
+	document.getElementById('bio').style.display = 'none';
+	document.getElementById('nav-pro').className = 'active';
+	document.getElementById('nav-bio').className = '';
+}
+
+function displayBio()
+{
+	document.getElementById('project-holder').style.display = 'none';
+	document.getElementById('bio').style.display = 'block';
+	document.getElementById('nav-pro').className = '';
+	document.getElementById('nav-bio').className = 'active';
 }
