1a8e3fbb479e0d8397b931157c7bc9894b5060ae
tsamridh86 <t.samridh.86@gmail.com>
2022-01-07 11:53:01 +0545

    Update

--- src/components/HeaderSection/HeaderInfoSection.tsx
+++ src/components/HeaderSection/HeaderInfoSection.tsx
@@ -1,15 +1,29 @@
+import CakeIcon from '@mui/icons-material/Cake';
+
 const nameStyle = {
 	fontSize: "3em",
 	fontFamily: "Roboto",
 	fontWeight: "bold",
 }
 
+const designationStyle = {
+	fontSize: "1em",
+	fontFamily: "Roboto",
+	fontWeight: "boldest",
+}
 
+const bDayStyle = {
+	fontSize: "0.8em",
+	fontFamily: "Roboto",
+	fontWeight: "boldest",
+}
 const HeaderInfoSection = () => {
 	return <>
 		<div style={nameStyle}>Samridh Tuladhar</div>
-		<div>Computer Engineer, Full Stack Web Developer</div>
-		<div>12th Nov. 1996</div>
+		<div style={designationStyle}>Computer Engineer, Full Stack Web Developer</div>
+		<div style={bDayStyle}>12th Nov 1996</div>
+		<br />
+		<div>A computer engineer with a passion for environmentally friendly automation and disdain for paperwork.</div>
 	</>
 }
 
--- src/components/SelectionSection/SelectionSectionContainer.tsx
+++ src/components/SelectionSection/SelectionSectionContainer.tsx
@@ -1,5 +1,14 @@
+import { AppBar, Button, Container } from "@mui/material"
+
 const SelectionSectionContainer = () => {
-    return<>Hi I'm the selection section</>
+  return <AppBar position="static" color="transparent">
+    <Container maxWidth="xl">
+      <Button>Experience</Button>
+      <Button>Education</Button>
+      <Button>Other projects</Button>
+      <Button>Contact</Button>
+    </Container>
+  </AppBar>
 }
 
 export default SelectionSectionContainer
